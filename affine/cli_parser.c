a0e91d86f4fe85aa781cb242d3d1640a7cb7b782
Nathan Ferreira <nferreira1729@gmail.com>
2024-09-14 10:15:56 -0400

    implement cli structure, stop tracking object files

--- affine/cli_parser.c
+++ affine/cli_parser.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "cli_parser.h"
 
+const char *command_strings[COMMAND_COUNT] = {
+    "decrypt", "encrypt", "set-key", "read-key", "help"
+};
+
 static void print_info_screen() {
-  fprintf(stdout, "Usage: affine  <command>  <command_args>\n\n"
-      "Valid commands:\n"
-      "  decrypt\n"
-      "  encrypt\n"
-      "  set-key\n"
-      "  read-key\n"
-  );
+  unsigned i;
+  fprintf(stdout, "Usage: affine  <command>  <command_args>\n\nValid commands:\n");
+
+  for (i=0; i<COMMAND_COUNT; i++) {
+    fprintf(stdout, "  %s\n", command_strings[i]);
+  }
 }
 
 void parse_command_line(int argc, char **argv) {
   if (argc == 1) {
     print_info_screen();
     return;
+  }
+
+  if (!strncmp(command_strings[COMMAND_DECRYPT], argv[1], 10)) {
+    /* TODO decryption */
+    fprintf(stdout, "You selected decryption\n");
+  } else if (!strncmp(command_strings[COMMAND_ENCRYPT], argv[1], 10)) {
+    /* TODO encryption */
+    fprintf(stdout, "You selected encryption\n");
+  } else if (!strncmp(command_strings[COMMAND_SET_KEY], argv[1], 10)) {
+    /* TODO set-key */
+    fprintf(stdout, "You selected set-key\n");
+  } else if (!strncmp(command_strings[COMMAND_READ_KEY], argv[1], 10)) {
+    /* TODO read-key */
+    fprintf(stdout, "You selected read-key\n");
+  } else if (!strncmp(command_strings[COMMAND_HELP], argv[1], 10)) {
+    print_info_screen(); 
+  } else {
+    fprintf(stderr, "Invalid command: %s, is not recognized\n\n", argv[1]);
+    print_info_screen();
   }
 }
--- affine/cli_parser.h
+++ affine/cli_parser.h
@@ -1,6 +1,17 @@
 #ifndef CLI_PARSER_H
 #define CLI_PARSER_H
 
+#define COMMAND_COUNT 5
+
+typedef enum {
+  COMMAND_DECRYPT,
+  COMMAND_ENCRYPT,
+  COMMAND_SET_KEY,
+  COMMAND_READ_KEY,
+  COMMAND_HELP
+} Command;
+
+extern const char* command_strings[COMMAND_COUNT];
 
 void parse_command_line(int argc, char **argv);
 
